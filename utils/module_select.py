83bb7e729de5d9af17ece5a55b1c935da03e3a61
HanHyuk <nalnez13@fssolution.co.kr>
2021-09-08 08:09:14 +0000

    train

---
+++ utils/module_select.py
@@ -0,0 +1,12 @@
+from torch import optim
+from models.frostnet import FrostNet
+
+
+def get_model(model_name):
+    model_dict = {'FrostNet': FrostNet}
+    return model_dict.get(model_name)
+
+
+def get_optimizer(optimizer_name):
+    optim_dict = {'sgd': optim.SGD(), 'adam': optim.Adam}
+    return optim_dict.get(optimizer_name)
