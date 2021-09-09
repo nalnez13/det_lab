35a554b2547d1f73783d577db68466b0305d8eb0
HanHyuk <nalnez13@fssolution.co.kr>
2021-09-09 01:58:45 +0000

    pasre scientific notation

--- utils/yaml_helper.py
+++ utils/yaml_helper.py
@@ -1,10 +1,23 @@
-from torch.serialization import load
+import re
 import yaml
+
+
+__loader = yaml.SafeLoader
+__loader.add_implicit_resolver(
+    u'tag:yaml.org,2002:float',
+    re.compile(u'''^(?:
+     [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+    |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+    |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+    |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+    |[-+]?\\.(?:inf|Inf|INF)
+    |\\.(?:nan|NaN|NAN))$''', re.X),
+    list(u'-+0123456789.'))
 
 
 def load_yaml_file(file):
     with open(file, 'r') as f:
-        return yaml.safe_load(f)
+        return yaml.load(f, Loader=__loader)
 
 
 def get_train_configs(file=None):
