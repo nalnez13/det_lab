b236b1afe4bef97e56b46dc63cd5edea68cd07b6
한혁 <nalnez13@fssolution.co.kr>
2021-10-07 15:27:43 +0900

    mAP validation 추가

--- module/detector.py
+++ module/detector.py
@@ -1,16 +1,20 @@
-from models.loss.multibox_loss import MultiBoxLoss
 import pytorch_lightning as pl
+from models.loss.multibox_loss import MultiBoxLoss
+from utils.mAP_evaluate import DetectionMAP
 from utils.module_select import get_optimizer
+
 from module.lr_scheduler import CosineAnnealingWarmUpRestarts
+from module.transformer import Transformer
 
 
 class Detector(pl.LightningModule):
     def __init__(self, model, cfg, epoch_length=None):
         super().__init__()
-        self.model = model
         self.save_hyperparameters(ignore='model')
-
+        self.model = model
+        self.transformer = Transformer()
         self.loss_fn = MultiBoxLoss()
+        self.mAP = DetectionMAP(cfg['classes'])
 
     def forward(self, x):
         cls_pred, reg_pred = self.model(x)
@@ -24,13 +28,38 @@ class Detector(pl.LightningModule):
                  logger=True, on_step=True, on_epoch=True)
         return loss
 
+    def on_validation_epoch_start(self):
+        self.mAP.reset_accumulators()
+
     def validation_step(self, batch, batch_idx):
         cls_pred, reg_pred = self.model(batch['img'])
         loss = self.loss_fn([cls_pred, reg_pred, batch])
 
         self.log('val_loss', loss, logger=True, on_epoch=True)
+        confidences, cls_idxes, boxes = self.transformer(
+            [batch['img'], cls_pred, reg_pred])
 
+        for b in range(len(confidences)):
+            gt_annots = batch['annot'][b]
+            mask = gt_annots[:, -1] != -1
+            gt_boxes = gt_annots[mask, :4]
+            gt_cls = gt_annots[mask, -1]
+
+            data = (
+                boxes[b].cpu().numpy(),
+                cls_idxes[b].cpu().numpy(),
+                confidences[b].cpu().numpy(),
+                gt_boxes.cpu().numpy(),
+                gt_cls.cpu().numpy()
+            )
+            self.mAP.evaluate(*data)
         return loss
+
+    def on_validation_epoch_end(self) -> None:
+        ap_per_class, mAP = self.mAP.compute_map()
+        self.log('val_mAP', mAP, on_epoch=True, prog_bar=True)
+        for k, v in ap_per_class.items():
+            self.log(f'val_AP_{k}', v, on_epoch=True)
 
     def configure_optimizers(self):
         cfg = self.hparams.cfg
