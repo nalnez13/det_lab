b39315c7411c377bc426f099e11b0f2f618e810a
한혁 <nalnez13@fssolution.co.kr>
2021-10-12 09:48:46 +0900

    gradient clip

--- train_detector.py
+++ train_detector.py
@@ -65,6 +65,7 @@ def train(cfg):
         plugins=DDPPlugin(
             find_unused_parameters=False) if platform.system() != 'Windows' else None,
         callbacks=callbacks,
+        gradient_clip_val=0.1,
         **cfg['trainer_options'])
     trainer.fit(model_module, data_module)
 
